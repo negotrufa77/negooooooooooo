3fd28494944c6b6fa60b5cb050ca39ed9b84542e
negotrufa77 <mateus.rodrigues.erik@escola.pr.gov.br>
2025-07-07 08:17:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,57 @@
+criaCartao(
+    'Lingua inglesa',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    ' Monte Everest'
+)
+
+criaCartao(
+    'Programação',
+    ' Onde se localiza Machu Picchu?',
+    ' Peru?'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    ' Que país tem o formato de uma bota?',
+    'Itália'
+)
+criaCartao(
+    'Programação',
+    'Quem inventou a lâmpada?',
+    'Thomas Edison'
+)
+
+criaCartao(
+    'Geografia',
+    'Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?',
+    'Aproximadamente 24 horas.'
+)
+
+criaCartao(
+    'Programação',
+    ' A que temperatura a água ferve?',
+    ' 100 ºC.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Quais são as fases da Lua?',
+    'Nova, crescente, cheia e minguante'
+)
+criaCartao(
+    'Programação',
+    'Quantos ossos temos no nosso corpo?',
+    '206.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    ' Qual o maior planeta do sistema solar?',
+    'Júpiter'
+)
